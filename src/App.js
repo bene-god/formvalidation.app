cc612c7de1f0ad6b72f60f2c77f4dceb90b657e1
bene-god <egbadonjeffrey@gmail.com>
2022-02-27 13:02:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import FormInput from "./components/FormInput";
 
 function App() {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "username",
+      errorMessage:
+        "username must be valid characters and not contain any special characters",
+      label: "username",
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "text",
+      placeholder: "Email",
+      errorMessage: "It should be a valid email address!",
+      label: "Email",
+    },
+    {
+      id: 3,
+      name: "birthday",
+      type: "date",
+      placeholder: "Birthday",
+      label: "Birthday",
+    },
+    {
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      errorMessage: "Passwords must be more than 8 characters",
+      label: "Password",
+    },
+    {
+      id: 5,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Confirm Password",
+      errorMessage: "Passwords dont' match",
+      label: "Confirm Password",
+    },
+  ];
+
+  // console.log("re-rendered");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    console.log(Object.fromEntries(data.entries()));
+  };
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  console.log(values);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+
+        <button> Submit </button>
+      </form>
     </div>
   );
 }
---
+++ src/components/FormInput.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./FormInput.css";
+
+const FormInput = (props) => {
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+  return (
+    <div className="formInput">
+      <label> {label} </label>
+      <input {...inputProps} onChange={onChange} />
+      <span> Form Input </span>
+    </div>
+  );
+};
+
+export default FormInput;
